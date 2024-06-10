bec84107745e29837b55f43c91eb8e1a41501f3b
digidagsOrg <107912351+digidagsOrg@users.noreply.github.com>
2024-06-10 15:47:17 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,16 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 13) {
+        position = ServoSlow.setServoAngle(1, 90, 10, position, ServoSlow.ServoDirection.CW)
+    } else if (receivedNumber == 14) {
+        position = ServoSlow.setServoAngle(1, 90, 10, position, ServoSlow.ServoDirection.CCW)
+    } else if (receivedNumber == 0) {
+        ServoSlow.setServoPosition(1, 90)
+        position = 90
+    } else {
+    	
+    }
+})
+let position = 0
+position = 90
+radio.setGroup(1)
+ServoSlow.setServoPosition(1, 90)
